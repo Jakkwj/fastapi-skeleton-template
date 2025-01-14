55788aa7f6bcd66a9a054c58aa4f429ed842b54c
Jakkwj <shutupwj@163.com>
2025-01-14 18:50:00 +0800

    update exception handler 2

--- fastapi-skeleton-template/app/providers/exception_handler.py
+++ fastapi-skeleton-template/app/providers/exception_handler.py
@@ -1,4 +1,5 @@
 from traceback import format_exception
+
 from fastapi import (
     FastAPI,
     Request,
@@ -26,37 +27,25 @@ def register(
     app: FastAPI, settings: DevelopmentSettings | ProductionSettings
 ):  # settings: placeholder
     @app.exception_handler(AuthenticationError)
-    async def authentication_exception_handler(
-        request: Request, error: AuthenticationError
-    ):
+    async def authentication_exception_handler(request: Request, error: AuthenticationError):
         """
         认证异常处理
         """
-        return ORJSONResponse(
-            status_code=error.status_code, content={"message": error.message}
-        )
+        return ORJSONResponse(status_code=error.status_code, content={"message": error.message})
 
     @app.exception_handler(AuthorizationError)
-    async def authorization_exception_handler(
-        request: Request, error: AuthorizationError
-    ):
+    async def authorization_exception_handler(request: Request, error: AuthorizationError):
         """
         权限异常处理
         """
-        return ORJSONResponse(
-            status_code=error.status_code, content={"message": error.message}
-        )
+        return ORJSONResponse(status_code=error.status_code, content={"message": error.message})
 
     @app.exception_handler(RequestValidationError)
-    async def validation_exception_handler(
-        request: Request, exc: RequestValidationError
-    ):
+    async def validation_exception_handler(request: Request, exc: RequestValidationError):
         """
         参数验证异常
         """
-        tb = "".join(
-            format_exception(type(exc), exc, exc.__traceback__)
-        )  # 获取完整的堆栈跟踪信息
+        tb = "".join(format_exception(type(exc), exc, exc.__traceback__))  # 获取完整的堆栈跟踪信息
         logger.error(f"Request to {request.url} caused an exception:\n{tb}")
 
         return ORJSONResponse(
@@ -71,9 +60,7 @@ def register(
 
     @app.exception_handler(StarletteHTTPException)
     async def custom_http_exception_handler(request: Request, exc):
-        tb = "".join(
-            format_exception(type(exc), exc, exc.__traceback__)
-        )  # 获取完整的堆栈跟踪信息
+        tb = "".join(format_exception(type(exc), exc, exc.__traceback__))  # 获取完整的堆栈跟踪信息
         logger.error(f"Request to {request.url} caused an exception:\n{tb}")
 
         return ORJSONResponse(
@@ -81,7 +68,6 @@ def register(
             content=jsonable_encoder(
                 {
                     "message": f"{exc.detail}. ({str(request.url).replace(get_router_root(request.url), '')})",
-                    "body": exc.body,
                 }
             ),
         )
